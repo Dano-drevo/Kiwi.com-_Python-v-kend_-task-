49250edad7c078d72753ebb77b20f5fd79f2d064
Daniel Laktič <daniel.laktic@gmail.com>
2018-01-11 03:33:27 +0100

    Update places.py

--- places.py
+++ places.py
@@ -2,31 +2,26 @@
 This module is designed to get geographic coordinate system from location's english name.
 It uses kiwi.com API named 'places'.
 """
-
 import requests
 import os
-def places(destination):
-    r = requests.get("https://locations.skypicker.com/locations?term=" + destination + "&locale=en&v=2")
-    lst = r.json()
+def places(destination,strng):
+    url="https://api.skypicker.com/locations?term=" + destination + "&locale=en"#&location_types="+strng
+    r = requests.get(url)
+    lst = r.json()['locations']
     cities = []
     for item in lst:
-        if item["type"] == 2:  #we are interested only in the cities, not the airports
+        if item["type"] == strng:  #we are interested only in the cities, not the airports
             cities.append(item)
+         
     if cities == []:
         os.system('clear')
         print('You have to choose existing city with existing airport, otherways you will stay home.\nOr you can use AITA airport code.')
         return False
-    population = 0
-    for city in cities:
-        try:
-            if population < city['population']: #choosing city with greatest population by default
-                result = city
-                country = city['parentId']
-        except:
-            pass
-    if population == 0:
-        city = cities[0]
-    coordinates = (result['lat'] ,result['lng'])
+    coordinates = (cities[0]['location']['lat'],cities[0]['location']['lon'])
+    if strng=='airport':
+        city_name, country_name = (cities[0]['city']['name'] , cities[0]['city']['country']['name'])
+    elif strng=='city':
+        city_name, country_name = (cities[0]['name'] , cities[0]['country']['name'])
     coordinates = round(coordinates[0],2) , round(coordinates[1],2)
-    coordinates = str(coordinates[0])+"-" + str(coordinates[1])+"-250km"
-    return coordinates, country
+    coordinates = str(coordinates[0])+"-" + str(coordinates[1])+'-100km'
+    return coordinates, city_name, country_name
